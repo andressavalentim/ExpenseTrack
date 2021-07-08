d6435ba364052c50075b1e77c0cce1ff3a8c87d7
Andressa Lucia Valentim <al_valentim@apple.com>
2021-07-08 13:37:18 -0300

    value bug fix

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -14,7 +14,7 @@ const ExpensesList = (props) => {
     <ExpenseItem 
     key={expense.id}
     title={expense.title}
-    amount ={expense.amout}
+    amount ={expense.amount}
     date ={expense.date}
     />
     ))}
